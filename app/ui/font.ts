c6157d367390c973e802803e34e02938e255f2b7
RajinAIDeveloper <abdullahalrajinaideveloper@gmail.com>
2025-01-18 21:50:31 +0600

    first commit

---
+++ app/ui/font.ts
@@ -0,0 +1,9 @@
+import  { Inter, Lusitana, EB_Garamond } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: [
+    '400', '700'] });
+
+export const ebgaramond = EB_Garamond({ subsets: ['latin'], weight: [
+    '400', '700'] });
